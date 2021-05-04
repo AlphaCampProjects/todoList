c5e85ecbc4902b3c60792430d939326db94a2515
tinahung126 <tina126hung@gmail.com>
2021-05-04 14:17:04 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
 // 載入 express 並建構應用程式伺服器
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
+
 const exphbs = require("express-handlebars");
-// 引用 body-parser
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 
 const routes = require("./routes");
+require("./config/mongoose");
 
+const app = express();
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 
 app.use(methodOverride("_method"));
-
-// 設定連線到 mongoDB
-mongoose.connect("mongodb://localhost/todo-list", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// 取得資料庫連線狀態
-const db = mongoose.connection;
-
-// 連線異常
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-
-// 連線成功
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
 
 //template
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
